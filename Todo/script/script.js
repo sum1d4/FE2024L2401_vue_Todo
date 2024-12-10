e1ee49190dad85c20f50f8ec6c1cea99cbcf0537
sum1d4 <2301171@s.asojuku.ac.jp>
2024-12-10 09:45:27 +0900

    Initial commit

---
+++ Todo/script/script.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      //データの準備
+
+    };
+  },
+  methods: {
+    //inputTextメソッドを追加
+
+    //addTodoメソッドを追加
+
+    //resetTextメソッドを追加
+    
+  }
+});
